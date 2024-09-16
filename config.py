7f9fdaf502eb063c099bcd89f385230e0280fd8e
Denis Afanasev <Denis.Afanasev@gmail.com>
2024-09-16 09:44:15 +0100

    move to GPT-4o-mini

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 KEYWORDS = ("игорь,", "igor,", "пес,", "@igorva_dev_bot", "@igorva_bot")
-BOT_VERSION = '3.0.1'
+BOT_VERSION = '3.0.2'
 CONFIG_FILE_NAME = "../config.yaml"
 SYSTEM_STATE_FILE_NAME = "../system_state.dump"
 WHITE_LIST_FILE_NAME = "../donaters.list"
